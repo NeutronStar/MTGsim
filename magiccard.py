b4de0e5dffa090d09ffc354182f7d59b87110044
Christopher Hamblin <chrishamblin7@Christopher-Hamblins-MacBook-Pro.local>
2015-08-02 23:22:48 -0400

    Add manacost parameter to magiccard class

--- magiccard.py
+++ magiccard.py
@@ -2,12 +2,15 @@ from card import Card
 
 class MagicCard(Card):
 
-    def __init__(self, name, type, subtype=None):
+    def __init__(self, name, type, manacost, subtype=None):
         self.name = name
         self.type = type
         self.subtype = subtype
+        self.manacost = manacost
 
     def info(self):
         print("Name:", self.name) 
         print("Type:", self.type)
         print("Subtype:", self.subtype)
+        print("manacost", self.manacost.string())
+
